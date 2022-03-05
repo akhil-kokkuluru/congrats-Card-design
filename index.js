02f648628f92e6339f81899ecdf8835700088788
akhil <akhilr37@gmail.com>
2022-03-05 12:35:35 +0000

    interface design completed

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const element = (
-    // Write your code here.
-  )
-  
-ReactDOM.render(element, document.getElementById('root'))
+  <div className="totalBg">
+    <p className="headingEl">Congratulations</p>
+    <div className="innerBg">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="images"
+      />
+      <h1 className="kiranname">Kiran V</h1>
+      <p className="bhimavaram">
+        Vishnu Institute of Computer Education and Technology <br /> Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="images"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
